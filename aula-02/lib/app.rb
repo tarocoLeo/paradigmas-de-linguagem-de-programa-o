86228226328efdf837ae98b4fb1a92f1c3f065b8
Leonardo Santana <leonardo.santana@locaweb.com.br>
2022-11-23 14:14:47 -0300

    aula 2

---
+++ aula-02/lib/app.rb
@@ -0,0 +1,31 @@
+class Itens
+
+end
+
+class cliente
+  nome = "nome"
+  pontos = 0
+end
+
+class Pedido
+  def adicionaItem(item, itens)
+    if itens == nil
+      itens = {}
+    end
+
+
+    
+  end
+
+  def aplicaDesconto(valorTotal)
+    valorTotal = valorTotal - valorTotal * 0.05
+
+  def calculaPontos(cliente, )
+
+  end
+
+
+end
+
+puts "Prezado cliente, informe o seu nome: "
+nome_cliente gets.chomp
